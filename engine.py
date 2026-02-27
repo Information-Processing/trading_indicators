0ca5ae85f9f1fb8e316918388338ca5f3a391a0c
vortexisalpha <joshuaboshua250505@gmail.com>
2026-02-27 20:44:40 +0000

    test on 10 30 60 vwma

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from binance_ws import BinanceWSClient
 from calc_engine import CalculationEngine 
 import time
 
+
 class Engine:
     def __init__(self):
         self.binance_ws = BinanceWSClient()
@@ -19,13 +20,17 @@ class Engine:
         while(1):
             now = time.time()
             
-            trades = self.binance_ws.get_trades_since(now - 11.0)
+            trades_10 = self.binance_ws.get_trades_since(now - 11.0)
+            trades_30 = self.binance_ws.get_trades_since(now - 31.0)
+            trades_60 = self.binance_ws.get_trades_since(now - 61.0) 
 
-            vwma_10 = self.ce.vwma_calculate(trades, now, 10.0)
+            vwma_10 = self.ce.vwma_calculate(trades_10, now, 10.0)
+            vwma_30 = self.ce.vwma_calculate(trades_30, now, 30.0)
+            vwma_60 = self.ce.vwma_calculate(trades_60, now, 60.0) 
 
             time.sleep(1)
 
-            print(vwma_10)
+            print(f"VWMA [10 sec: {vwma_10}], [30 sec: {vwma_30}], [60 sec: {vwma_60}]")
 
 
         
