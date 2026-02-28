8a35526ca4e2e02f3c2ba0a9b46277cf59aa280a
vortexisalpha <joshuaboshua250505@gmail.com>
2026-02-28 18:42:27 +0000

    added unoptimised to engine

--- liner_regression/lr_engine.py
+++ liner_regression/lr_engine.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 from liner_regression.software_optimised_liner_regression import OptimisedSoftwareLR
+from liner_regression.unoptimised_lr import OptimisedSoftwareLR, UnoptimisedSoftwareLR
 
 # example input:
 # [[1,2,3,4,5],
@@ -39,11 +40,14 @@ class LinearRegressionEngine:
         collumn_headers = list(FEATURE_RANGES.keys()) + ['1']
         initial_input_data = generate_random_data(50)
 
-        self.optimised_sw_lr =  self.initialise_optinised_sw_lr(initial_input_data, collumn_headers)
+        self.optimised_sw_lr =  self.initialise_optimised_sw_lr(initial_input_data, collumn_headers)
+        self.unoptimised_sw_lr =  self.initialise_unoptimised_sw_lr(initial_input_data, collumn_headers)
 
-    def initialise_optinised_sw_lr(self, data_in, collumn_headers):
+    def initialise_optimised_sw_lr(self, data_in, collumn_headers):
         return OptimisedSoftwareLR(data_in, collumn_headers)
 
+    def initialise_unoptimised_sw_lr(self, data_in, collumn_headers):
+        return UnoptimisedSoftwareLR(data_in, collumn_headers)
     
 
 
